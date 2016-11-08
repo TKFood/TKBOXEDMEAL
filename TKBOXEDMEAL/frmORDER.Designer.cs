b422390038305a0762661ceff0b68f6944b74241
at3158 <tk290@tkfood.com.tw>
2016-11-08 14:22:25 +0800

    V20161108

--- TKBOXEDMEAL/frmORDER.Designer.cs
+++ TKBOXEDMEAL/frmORDER.Designer.cs
@@ -173,7 +173,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(61, 12);
             this.label2.TabIndex = 14;
-            this.label2.Text = "V20161107";
+            this.label2.Text = "V20161108";
             // 
             // dataGridView1
             // 
