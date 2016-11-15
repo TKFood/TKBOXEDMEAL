8a5b498104d3b0f41be350331c71a883faa97c60
at3158 <tk290@tkfood.com.tw>
2016-11-15 08:56:11 +0800

    modifer

--- TKBOXEDMEAL/frmORDER.Designer.cs
+++ TKBOXEDMEAL/frmORDER.Designer.cs
@@ -172,9 +172,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(803, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 12);
+            this.label2.Size = new System.Drawing.Size(55, 12);
             this.label2.TabIndex = 14;
-            this.label2.Text = "V20161108";
+            this.label2.Text = "V2016115";
             // 
             // dataGridView1
             // 
